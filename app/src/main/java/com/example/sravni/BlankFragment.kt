df2732d01b53b080269c651a9456c13627bc8272
aleksej <allozvr-al@mail.ru>
2022-02-21 05:00:06 +0300

    "Кажется я по ошибке не то нажал, и коммит ушел не на ту ветку"

--- app/src/main/java/com/example/sravni/BlankFragment.kt
+++ app/src/main/java/com/example/sravni/BlankFragment.kt
@@ -1,31 +1,36 @@
 package com.example.sravni
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_LEGACY
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import com.example.sravni.databinding.FragmentBlankBinding
+import com.example.sravni.ui.main.Showbottom
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BlankFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding : FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var showbottom: Showbottom
+    private var linearVisible = "linearlayoutvisible"
+    private var imageID = "imageID"
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(linearVisible, binding.lin.isVisible)
+//        outState.putInt(imageID, binding.imageView4.resources.)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -33,27 +38,66 @@ class BlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textView17.text = Html.fromHtml(getString(R.string.welcome_messages,
+            "БТ","КМ","КТ","КБМ","КО","КВС"), FROM_HTML_MODE_LEGACY)
+        binding.constr.setOnClickListener {
+            hideshow()
+        }
+        showbottom = Showbottom()
+        binding.city.setOnClickListener {
+            showbottom.onClick(it)
+        }
+        binding.horsepowers.setOnClickListener {
+            showbottom.onClick(it)
+        }
+        binding.drivers.setOnClickListener {
+            showbottom.onClick(it)
+        }
+        binding.age.setOnClickListener {
+            showbottom.onClick(it)
+        }
+        binding.minimum.setOnClickListener {
+            showbottom.onClick(it)
+        }
+        binding.incidentless.setOnClickListener {
+            showbottom.onClick(it)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (savedInstanceState!=null){
+            binding.lin.isVisible = savedInstanceState.getBoolean(linearVisible, false)
+            if (binding.lin.isVisible == true){
+                binding.imageView4.setImageResource(R.drawable.img_3)
+            }
+            else
+                binding.imageView4.setImageResource(R.drawable.img_2)
+        }
+    }
+    fun hideshow(){
+        var animation = AnimationUtils.loadAnimation(context, R.anim.alpha)
+        if (binding.lin.visibility == View.GONE){
+            binding.lin.visibility = View.VISIBLE
+            binding.imageView4.setImageResource(R.drawable.img_3)
+            binding.lin.startAnimation(animation)
+        }
+        else{
+            animation = AnimationUtils.loadAnimation(context, R.anim.alpha1)
+            binding.lin.visibility = View.GONE
+            binding.imageView4.setImageResource(R.drawable.img_2)
+            binding.lin.startAnimation(animation)
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = BlankFragment()
     }
 }
---
+++ app/src/main/java/com/example/sravni/ui/main/Showbottom.kt
@@ -0,0 +1,20 @@
+package com.example.sravni.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import com.example.sravni.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+
+class Showbottom: View.OnClickListener{
+    override fun onClick(v: View?) {
+        val dialog = v.let { BottomSheetDialog(v!!.context) }
+        println("!!!!!")
+
+        val view = LayoutInflater.from(v?.context).inflate(R.layout.bottom_sheet, null)
+        dialog.setCancelable(true)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setContentView(view)
+        dialog.show()
+    }
+}
